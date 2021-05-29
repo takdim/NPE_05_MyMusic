363bd7d44998341394f16234a30d163ea5c61bf9
takdim <takdim253@gmail.com>
2021-05-29 12:53:06 +0800

    add login bede

--- app/src/main/java/com/example/npe_05_mymusic/Activities/LoginActivity.java
+++ app/src/main/java/com/example/npe_05_mymusic/Activities/LoginActivity.java
@@ -1,16 +1,103 @@
 package com.example.npe_05_mymusic.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.npe_05_mymusic.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView etEmail, etPassword;
+    private Button btnLogin;
+    private ImageButton ibBack;
+    private FirebaseAuth mAuth;
+    private  FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        ibBack = (ImageButton) findViewById(R.id.ib_back);
+        etEmail = (EditText) findViewById(R.id.et_username);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        btnLogin = (Button) findViewById(R.id.btn_masuk);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser u = LoginActivity.this.mAuth.getCurrentUser();
+                if (u != null) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        };
+
+        ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, WelcomeActivity.class));
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin();
+            }
+        });
+    }
+
+    public void userLogin(){
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+
+        if (email.isEmpty()){
+            etEmail.setError("Email is required");
+            etEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("please provide valid email");
+            etEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            etPassword.setError("Password is required");
+            etPassword.requestFocus();
+        }
+        if (password.length() < 6){
+            etPassword.setError("password must be 6 character");
+            etPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }else {
+                            Toast.makeText(LoginActivity.this, "failed login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/npe_05_mymusic/Activities/WelcomeActivity.java
+++ app/src/main/java/com/example/npe_05_mymusic/Activities/WelcomeActivity.java
@@ -2,15 +2,48 @@ package com.example.npe_05_mymusic.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.npe_05_mymusic.R;
 
-public class WelcomeActivity extends AppCompatActivity {
-
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnDaftar, btnNoHp, btnGoogle;
+    private TextView tvMasuk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        btnDaftar = (Button) findViewById(R.id.btn_daftar);
+        btnNoHp = (Button) findViewById(R.id.btn_nohp);
+        btnGoogle = (Button) findViewById(R.id.btn_noemail);
+        tvMasuk = (TextView) findViewById(R.id.tv_masuk);
+
+        btnNoHp.setOnClickListener(this);
+        btnDaftar.setOnClickListener(this);
+        btnGoogle.setOnClickListener(this);
+        tvMasuk.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_masuk:
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+                break;
+            case R.id.btn_noemail:
+
+                break;
+            case R.id.btn_nohp:
+
+                break;
+            case R.id.btn_daftar:
+                startActivity(new Intent(WelcomeActivity.this, RegisterActivity.class));
+                break;
+        }
     }
 }
