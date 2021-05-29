afbb71162a3db043061922a9fee22adefbbbac72
takdim <takdim253@gmail.com>
2021-05-29 15:26:44 +0800

    handle clicked in profile fragment

--- app/src/main/java/com/example/npe_05_mymusic/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/npe_05_mymusic/Fragments/ProfileFragment.java
@@ -1,22 +1,71 @@
 package com.example.npe_05_mymusic.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.npe_05_mymusic.Activities.UploadActivity;
 import com.example.npe_05_mymusic.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
+    private TextView tvName, tvEdit;
+    private Button btnAdd;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        tvName = (TextView) view.findViewById(R.id.tv_name);
+        tvEdit = (TextView) view.findViewById(R.id.tv_edit);
+        reference = FirebaseDatabase.getInstance().getReference("User");
+        mAuth = FirebaseAuth.getInstance();
+        btnAdd = (Button) view.findViewById(R.id.btn_add);
+
+        reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("fullName").getValue(String.class);
+                tvName.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        btnAdd.setOnClickListener(this);
+        tvEdit.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_edit:
+                break;
+            case R.id.btn_add:
+                startActivity(new Intent(getContext(), UploadActivity.class));
+                break;
+        }
     }
 }
