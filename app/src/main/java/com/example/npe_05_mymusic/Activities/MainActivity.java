5ee3a47899876fe617b72db35292f2944ddabc89
takdim <takdim253@gmail.com>
2021-05-29 14:53:21 +0800

    add login fragments

--- app/src/main/java/com/example/npe_05_mymusic/Activities/MainActivity.java
+++ app/src/main/java/com/example/npe_05_mymusic/Activities/MainActivity.java
@@ -1,16 +1,79 @@
 package com.example.npe_05_mymusic.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.npe_05_mymusic.Fragments.FavoriteFragment;
+import com.example.npe_05_mymusic.Fragments.HomeFragment;
+import com.example.npe_05_mymusic.Fragments.ProfileFragment;
+import com.example.npe_05_mymusic.Fragments.SearchFragment;
 import com.example.npe_05_mymusic.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame_main, new HomeFragment()).commit();
+        }
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user == null){
+                    startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+                    finish();
+                }
+            }
+        };
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.menu_item_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.menu_item_favorite:
+                            selectedFragment = new FavoriteFragment();
+                            break;
+                        case R.id.menu_item_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.menu_item_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_main,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
--- app/src/main/java/com/example/npe_05_mymusic/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/npe_05_mymusic/Fragments/HomeFragment.java
@@ -2,13 +2,22 @@ package com.example.npe_05_mymusic.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.npe_05_mymusic.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +26,51 @@ import com.example.npe_05_mymusic.R;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private TextView tvName;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        tvName = (TextView) view.findViewById(R.id.tv_name);
+        reference = FirebaseDatabase.getInstance().getReference("User");
+        mAuth = FirebaseAuth.getInstance();
+
+        reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("fullName").getValue(String.class);
+                tvName.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return view;
+    }
+
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+
 }
